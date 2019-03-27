cabe843d71a83446a0bc160ddce95db9311406f8
Jenn <jenn.a.martinez@outlook.com>
2019-03-26 21:52:23 -0700

    edits

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,6 @@ var router = express.Router();
 // Routes //
 router.get("/", function(req, res) {
   res.redirect("/burgers");
-  
 });
   
 router.get("/burgers", function(req,res){
@@ -16,9 +15,20 @@ router.get("/burgers", function(req,res){
   });
 });
 
-router.post("/api/burgers", function(req, res) {
-  burger.create(["burger_name"], [req.body.burger_name], function(result) {
-    res.json({ id: result.insertId });
+// router.post("/api/burgers", function(req, res) {
+//   burger.create(["burger_name"], [req.body.burger_name], function(result) {
+//     res.json({ id: result.insertId });
+//       res.redirect("/burgers");
+//   });
+// });
+
+router.post("/burgers/create", function(req, res) {
+  // takes the request object using it as input for burger.addBurger
+  burger.create(req.body.burger_name, function(result) {
+    // wrapper for orm.js that using MySQL insert callback will return a log to console,
+    // render back to index with handle
+    console.log(result);
+    res.redirect("/");
   });
 });
 
